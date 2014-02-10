365996065882b8e8628f229108df8a7e7cee37bc
jresins <jresins@gmail.com>
2014-02-10 15:54:16 +0800

    add admin for Notice Model

--- notification/admin.py
+++ notification/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from notification.models import NoticeType, NoticeSetting, NoticeQueueBatch
+from notification.models import Notice
 
 
 class NoticeTypeAdmin(admin.ModelAdmin):
@@ -10,7 +11,11 @@ class NoticeTypeAdmin(admin.ModelAdmin):
 class NoticeSettingAdmin(admin.ModelAdmin):
     list_display = ["id", "user", "notice_type", "medium", "send"]
 
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = ['message', 'notice_type', 'recipient', 'unseen', 'added']
+    raw_id_fields = ('recipient',)
 
 admin.site.register(NoticeQueueBatch)
 admin.site.register(NoticeType, NoticeTypeAdmin)
 admin.site.register(NoticeSetting, NoticeSettingAdmin)
+admin.site.register(Notice, NoticeAdmin)
